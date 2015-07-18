c60c44157442e1af0a713f9f9b9ead627eed7567
lamazavr <mr.dimas@meta.ua>
2015-07-18 15:51:58 +0300

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gpio',
+    version='0.1',
+    description='Python GPIO control module',
+    author='Dmitrey Salnikov',
+    author_email='mr.dimas@meta.ua',
+    url='https://github.com/lamazavr/gpio/',
+    packages= [''],
+    license='MIT',
+)
